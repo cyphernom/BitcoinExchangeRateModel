bb663c19933918ecf52e4c366cf7585bad61ce57
Code::Orange <82763789+btcodeorange@users.noreply.github.com>
2023-07-21 00:30:58 +1000

    Update baerm.py
    refactored most of the code to functions. removed a unused imports. removed the other graphs and the backtesting. removed the GBM code.

--- baerm.py
+++ baerm.py
@@ -1,99 +1,15 @@
-#(c)2020-2023 Nick Phraudsta (btconometrics/codeorange)
+#(c)2020-2023 Nick Btconometrics
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 import datetime as dt
 from scipy.stats import norm
 from scipy import stats
-import seaborn as sns
-import matplotlib.widgets as widgets
 from matplotlib.widgets import Button
-import matplotlib
-matplotlib.use('TkAgg')
 from math import exp, cos, log
-
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from matplotlib.ticker import ScalarFormatter
-from scipy import stats
-# Import data
-url = "https://raw.githubusercontent.com/coinmetrics/data/master/csv/btc.csv"
-df = pd.read_csv(url)
-
-# Fix date formats and tsset the data
-df['date'] = pd.to_datetime(df['time'], format='%Y-%m-%d')
-df.drop('time', axis=1, inplace=True)
-
-# Extend the range
-df = df.append(pd.DataFrame({'date': pd.date_range(df['date'].max() + pd.Timedelta(days=1), periods=2000, freq='D')})).reset_index(drop=True)
-
-# Replace missing blkcnt values
-df.loc[df['BlkCnt'].isnull(), 'BlkCnt'] = 6 * 24
-
-# Generate sum_blocks and hp_blocks
-df['sum_blocks'] = df['BlkCnt'].cumsum()
-df['hp_blocks'] = df['sum_blocks'] % 210001
-
-# Generate hindicator and epoch
-df['hindicator'] = (df['hp_blocks'] < 200) & (df['hp_blocks'].shift(1) > 209000)
-df['epoch'] = df['hindicator'].cumsum() 
-
-# Generate reward and daily_reward
-df['reward'] = 50 / (2 ** df['epoch'].astype(float))
-df.loc[df['epoch'] >= 33, 'reward'] = 0
-df['daily_reward'] = df['BlkCnt'] * df['reward']
-
-# Generate tsupply
-df['tsupply'] = df['daily_reward'].cumsum()
-
-# Generate logprice
-df['logprice'] = np.log(df['PriceUSD'])
-
-# Calculate phaseplus variable
-start_date = pd.to_datetime('2008-10-31')
-df['days_since_start'] = (df['date'] - start_date).dt.days
-df['phaseplus'] = df['reward'] - (df['epoch'] + 1) ** 2
-
-# Define function to calculate Kelly criterion bet size
-def kelly_criterion(prob, odds):
-    return max(0, (prob * (odds + 1) - 1) / odds)
-
-# Drop rows with date < 2010-07-18
-df = df[df['date'] >= dt.datetime.strptime('2010-07-18', '%Y-%m-%d')].reset_index(drop=True)
-
-# Run the regression
-mask = df['epoch'] < 2
-reg_X = df.loc[mask, ['logprice', 'phaseplus']].shift(1).iloc[1:]
-reg_y = df.loc[mask, 'logprice'].iloc[1:]
-reg_X = sm.add_constant(reg_X)
-ols = sm.OLS(reg_y, reg_X).fit()
-coefs = ols.params.values
-
-
-# Step 1: Calculate AR + phase OLS
-start_date = pd.to_datetime('2010-07-30')
-mask = df['date'] > start_date
-indices = df[mask].index
-
-# Initialize YHAT with 0
-df['YHAT'] = df['logprice']
-
-# Calculate YHAT one row at a time
-for i in range(indices[0] + 1, indices[-1] + 1):
-    df.at[i, 'YHAT'] = coefs[0] + coefs[1] * df.at[i - 1, 'YHAT'] + coefs[2] * df.at[i, 'phaseplus']
-
-# Step 2: Calculate decayfunc
-n = df.index.to_numpy()
-df['decayfunc'] = 3 * np.exp(-0.0004 * n) * np.cos(0.005 * n - 1)
-
-# Step 3: Calculate prediction from Step 1 (already in YHAT column)
-
-# Step 4: Add decay to Step 3
-df['YHAT'] = df['YHAT'] + df['decayfunc']
-
-# Step 5: Exponentiate
-df['eYHAT'] = np.exp(df['YHAT'])
-
 
 def format_dollars(value, pos, is_minor=False):
     if value >= 1e9:
@@ -105,189 +21,203 @@ def format_dollars(value, pos, is_minor=False):
     else:
         s = '${:,.0f}'.format(value)
     return s
-    
-# Plot the results
-date_20190101 = dt.datetime.strptime('2011-01-01', '%Y-%m-%d')
-date_20240101 = dt.datetime.strptime('2028-01-01', '%Y-%m-%d')
-plot_df = df[(df['date'] > date_20190101) & (df['date'] < date_20240101)]
-
-# Calculate residuals
-residuals = plot_df['logprice'] - plot_df['YHAT']
-
-# Calculate the distribution function using the normal distribution
-cdf = norm.cdf(residuals, loc=residuals.mean(), scale=residuals.std())
-plot_df = plot_df.assign(cdf=cdf)
 
 
-
-# Create a 'signal' column to mark buy and sell points
-plot_df['signal'] = np.where(plot_df['cdf'] <= 0.4, 'buy', np.where(plot_df['cdf'] > 0.6, 'sell', 'hold'))
-
-# Filter buy and sell points from the second epoch onward
-buy_signal = plot_df[(plot_df['signal'] == 'buy') & (plot_df['epoch'] >= 2)]
-sell_signal = plot_df[(plot_df['signal'] == 'sell') & (plot_df['epoch'] >= 2)]
-
-
-# Prepare the figure with an additional subplot for the CDF
-#fig = plt.figure(figsize=(16, 10))
-
-# Main grid
-#gs_main = plt.GridSpec(3, 1, height_ratios=[3, 1, 1])  # Change the number of rows and height_ratios
-
-# Side grid
-#gs_side = plt.GridSpec(2, 2, width_ratios=[1, 1])
-fig, (ax1, ax5, ax6) = plt.subplots(3, 1, figsize=(16, 10), sharex=True)
-# Subplots
-#ax1 = plt.subplot(gs_main[0])
-#ax5 = plt.subplot(gs_main[1])
-#ax4 = plt.subplot(gs_main[2])  
-#ax2 = plt.subplot(gs_side[0])
-#ax3 = plt.subplot(gs_side[1])
-
-
-line1, = ax1.plot(plot_df['date'], plot_df['PriceUSD'], label='PriceUSD')
-line2, = ax1.plot(plot_df['date'], plot_df['eYHAT'], label='BAERM')
-ax1.set_yscale('log')
-ax1.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, pos: format_dollars(y, pos)[0]))
-ax1.yaxis.set_minor_formatter(ticker.FuncFormatter(lambda y, pos: format_dollars(y, pos, is_minor=True)[0]))
-ax1.tick_params(axis='y', which='minor', labelsize=8)
-ax1.set_xlabel('Date')
-ax1.set_ylabel('Exchange Rate')
-ax1.tick_params(axis='both', which='both', labelsize=12)
-
-# Add buy and sell signals (triangles) to the plot
-ax1.scatter(buy_signal['date'], buy_signal['PriceUSD'],
-            marker='^', color='g', label='Buy Signal')
-
-ax1.scatter(sell_signal['date'], sell_signal['PriceUSD'],
-            marker='v', color='r', label='Sell Signal')
-
-
-
-
-# Add dashed vertical lines at the change of each epoch
-epoch_changes = plot_df[plot_df['hindicator'] == True]
-for date in epoch_changes['date']:
-    ax1.axvline(x=date, color='blue', linestyle='-', linewidth=2)
-    
-minor_locator = ticker.LogLocator(subs=(0.2,0.4,0.6,0.8))
-ax1.yaxis.set_minor_locator(ticker.AutoMinorLocator(5))
-ax1.tick_params(axis='y', which='minor', labelsize=10)
-
-# Add grid lines to the plot
-ax1.grid(True)
-
-# Define function to update the y-axis scale
-def update_scale():
+def plot_charts(plot_df):
     global is_log_scale
-    if is_log_scale:
-        ax1.set_yscale('log')
-        ax1.yaxis.set_major_formatter(ticker.FuncFormatter(format_dollars))
-        button_scale.label.set_text('Switch to Linear Scale')
-    else:
-        ax1.set_yscale('linear')
-        ax1.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, _: f'${y:,.0f}'))
-        button_scale.label.set_text('Switch to Log Scale')
-    fig.canvas.draw()
+    gs_main = plt.GridSpec(1, 1)
+    fig, ax = plt.subplots(figsize=(8, 6))
 
-# Define function to toggle between linear and log scales
-def toggle_scale(event):
-    global is_log_scale
-    is_log_scale = not is_log_scale
+    # Calculate residuals
+    residuals = plot_df['logprice'] - plot_df['YHATs2']
+    residuals_std = np.std(residuals)
+
+    # Calculate likelihood thresholds
+    likelihoods = np.arange(0.3, .7, 0.01)
+    thresholds = norm.ppf(likelihoods, loc=0, scale=residuals_std)
+
+
+    # Calculate upper and lower bounds for each likelihood threshold
+    ub_lines = plot_df['YHATs2'].to_numpy()[:, np.newaxis] + thresholds
+    lb_lines = plot_df['YHATs2'].to_numpy()[:, np.newaxis] - thresholds
+
+    line1, = ax.plot(plot_df['date'], plot_df['PriceUSD'], label='PriceUSD')
+    line3, = ax.plot(plot_df['date'], plot_df['eYHATs2'], label='Phased BAERM')
+    
+    # Define a colormap for the filled areas
+    colormap = plt.cm.get_cmap('rainbow')
+    num_colors = len(likelihoods)
+    color_values = np.linspace(0, 1, num_colors)
+    
+    # Plot the lines representing different likelihoods and fill the gaps with colours
+    for i, likelihood in enumerate(likelihoods):
+        color = colormap(color_values[i])
+
+        if i < num_colors - 1:
+            next_color = colormap(color_values[i + 1])
+            ax.fill_between(
+                plot_df['date'],
+                np.exp(ub_lines[:, i]),
+                np.exp(ub_lines[:, i + 1]),
+                where=(np.exp(ub_lines[:, i]) >= np.exp(lb_lines[:, i])) & (np.exp(ub_lines[:, i + 1]) >= np.exp(lb_lines[:, i + 1])),
+                interpolate=True,
+                color=color,
+                alpha=0.3,
+            )
+            ax.fill_between(
+                plot_df['date'],
+                np.exp(lb_lines[:, i]),
+                np.exp(lb_lines[:, i + 1]),
+                where=(np.exp(ub_lines[:, i]) >= np.exp(lb_lines[:, i])) & (np.exp(ub_lines[:, i + 1]) >= np.exp(lb_lines[:, i + 1])),
+                interpolate=True,
+                color=color,
+                alpha=0.3,
+            )
+        
+    ax.set_yscale('log')
+    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, pos: format_dollars(y, pos)[0]))
+    ax.yaxis.set_minor_formatter(ticker.FuncFormatter(lambda y, pos: format_dollars(y, pos, is_minor=True)[0]))
+    ax.tick_params(axis='y', which='minor', labelsize=8)
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Exchange Rate')
+    ax.tick_params(axis='both', which='both', labelsize=12)
+
+    # Add dashed vertical lines at the change of each epoch
+    epoch_changes = plot_df[plot_df['hindicator'] == True]
+    for date in epoch_changes['date']:
+        ax.axvline(x=date, color='blue', linestyle='-', linewidth=2)
+        
+    minor_locator = ticker.LogLocator(subs=(0.2, 0.4, 0.6, 0.8))
+    ax.yaxis.set_minor_locator(ticker.AutoMinorLocator(5))
+    ax.tick_params(axis='y', which='minor', labelsize=10)
+
+    # Add grid lines to the plot
+    ax.grid(True)
+
+
+    
+    # Define function to update the y-axis scale
+    def update_scale():
+
+        if is_log_scale:
+            ax.set_yscale('log')
+            ax.yaxis.set_major_formatter(ticker.FuncFormatter(format_dollars))
+            button_scale.label.set_text('Switch to Linear Scale')
+        else:
+            ax.set_yscale('linear')
+            ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, _: f'${y:,.0f}'))
+            button_scale.label.set_text('Switch to Log Scale')
+        fig.canvas.draw()
+
+    # Define function to toggle between linear and log scales
+    def toggle_scale(event):
+        global is_log_scale
+        is_log_scale = not is_log_scale
+        update_scale()
+
+    # Add a button to toggle between linear and log scales
+    ax_button_scale = plt.axes([0.8, 0.025, 0.1, 0.04])
+    button_scale = Button(ax_button_scale, 'Switch to Log Scale')
+    is_log_scale = True
+    button_scale.on_clicked(toggle_scale)
     update_scale()
 
-# Add a button to toggle between linear and log scales
-ax_button_scale = plt.axes([0.8, 0.025, 0.1, 0.04])
-button_scale = Button(ax_button_scale, 'Switch to Log Scale')
-is_log_scale = True
-button_scale.on_clicked(toggle_scale)
-update_scale()
+    plt.show()
 
-# Plot the residuals vs fitted values
-#ax2.scatter(plot_df['eYHAT'], residuals, s=2)
-#ax2.axhline(y=0, color='black', linestyle='--')
-#ax2.set_xlabel('Fitted Values')
-#ax2.set_ylabel('Residuals')
+def calculate_oos_r_squared(actual, predicted):
+    residuals = actual - predicted
+    SSR = np.sum(residuals ** 2)
+    SST = np.sum((actual - actual.mean()) ** 2)
+    return 1 - SSR / SST
 
-# Plot a histogram of the residuals with overlaid standard normal distribution
-#sns.histplot(residuals, kde=True, ax=ax3, stat='density', color='skyblue')
-#x = np.linspace(-4, 4, 100)
-#ax3.plot(x, stats.norm.pdf(x, loc=residuals.mean(), scale=residuals.std()), color='red')
-#ax3.set_xlabel('Residuals')
-#ax3.set_ylabel('Density')
+def run_regression(df):
+    mask = df['epoch'] < 2
+    reg_X = df.loc[mask, ['logprice', 'phaseplus']].shift(1).iloc[1:]
+    reg_y = df.loc[mask, 'logprice'].iloc[1:]
+    reg_X = sm.add_constant(reg_X)
+    ols = sm.OLS(reg_y, reg_X).fit()
+    coefs = ols.params.values
+    return coefs, ols.summary()
 
-#ax3.set_xlim([-4, 4])
-#ax3.spines['top'].set_visible(False)
-#ax3.spines['right'].set_visible(False)
-#ax3.grid(axis='y')
-
-# Plot the probability density function as a time series
-#ax4.plot(plot_df['date'], plot_df['cdf'], label='Cumulative Distribution')
-#ax4.set_xlabel('Date')
-#ax4.set_ylabel('Cumulative Distribution')
+# Function to calculate YHAT one row at a time
+def calculate_YHAT(df, coefs):
+	df['YHAT'] = df['logprice']
+	for i in range(df.index[0] + 1, df.index[-1] + 1):
+		df.at[i, 'YHAT'] = coefs[0] + coefs[1] * df.at[i - 1, 'YHAT'] + coefs[2] * df.at[i, 'phaseplus']
 
 
 
-# Backtesting
-initial_capital = 1000
-capital = initial_capital
-position = 0
+def main():
+	#important dates:
+	phaseplus_start_date = pd.to_datetime('2008-10-31')
+	ar_start_date = pd.to_datetime('2010-07-30')
+	plot_start_date = dt.datetime.strptime('2011-01-01', '%Y-%m-%d')
+	plot_end_date = dt.datetime.strptime('2028-01-01', '%Y-%m-%d')  
+	
+	# Import data
+	url = "https://raw.githubusercontent.com/coinmetrics/data/master/csv/btc.csv"
+	df = pd.read_csv(url, low_memory=False)
+	
+	#data pre-processing
+	df['date'] = pd.to_datetime(df['time'], format='%Y-%m-%d')
+	df.drop('time', axis=1, inplace=True)
+	df = pd.concat([df, pd.DataFrame({'date': pd.date_range(df['date'].max() + pd.Timedelta(days=1), periods=15000, freq='D')})], ignore_index=True)
+	df.loc[df['BlkCnt'].isnull(), 'BlkCnt'] = 6 * 24
+	df['sum_blocks'] = df['BlkCnt'].cumsum()
+	df['hp_blocks'] = df['sum_blocks'] % 210001
+	df['hindicator'] = (df['hp_blocks'] < 200) & (df['hp_blocks'].shift(1) > 209000)
+	df['epoch'] = df['hindicator'].cumsum() 
+	df['reward'] = 50 / (2 ** df['epoch'].astype(float))
+	df.loc[df['epoch'] >= 33, 'reward'] = 0
+	df['daily_reward'] = df['BlkCnt'] * df['reward']
+	df['tsupply'] = df['daily_reward'].cumsum()
+	df['logprice'] = np.log(df['PriceUSD'])
+	
+	df['days_since_start'] = (df['date'] - phaseplus_start_date).dt.days
+	df['phaseplus'] = df['reward'] - (df['epoch'] + 1) ** 2
+	df = df[df['date'] >= dt.datetime.strptime('2010-07-18', '%Y-%m-%d')].reset_index(drop=True)
 
-backtest = pd.DataFrame(columns=['date', 'capital', 'position', 'signal'])
-# Filter plot_df to only include buy and sell signals
-trading_df = plot_df[plot_df['epoch'] >= 2]
+	coefs, ols_summary = run_regression(df)
 
-n_rows = len(plot_df)
-progress_step = n_rows // 50  # Set the progress step to print a hash mark every 2% of progress
+	print(coefs)
+	print(ols_summary)
 
-print("Backtest progress: ", end="")
-for index, row in trading_df.iterrows():
-    signal = row['signal']
-    price = row['PriceUSD']
-    cdf_value = row['cdf']
-    
-    if signal == 'buy' and capital > 0:
-        position_size = 1-cdf_value
-        position += capital * position_size / price
-        capital -= capital * position_size
+	#AR method:
+	# Step 1: Calculate AR + phase OLS	
+	mask = df['date'] > ar_start_date
+	indices = df[mask].index
 
-    elif signal == 'sell' and position > 0:
-        position_size = cdf_value
-        sell_amount = position * position_size
-        capital += sell_amount * price
-        position -= sell_amount
+	calculate_YHAT(df, coefs)
+	
+	# Step 2: Calculate decayfunc
+	n = df.index.to_numpy()
+	df['decayfunc'] = 3 * np.exp(-0.0004 * n) * np.cos(0.005 * n - 1)
+	
+	# Step 3: Calculate prediction from Step 1 (already in YHAT column)
+	# Step 4: Add decay to Step 3# Calculate the stock-to-flow ratio for each row
 
-    backtest = pd.concat([backtest, pd.DataFrame({
-        'date': [row['date']],
-        'capital': [capital],
-        'position': [position],
-        'signal': [signal],
-        'PriceUSD': [price]
-    })], ignore_index=True)
-    # Print a hash mark for every progress_step
-    if index % progress_step == 0:
-        print("#", end="", flush=True)
+	df['YHATs2'] = df['YHAT'] +df['decayfunc']
 
-print(" Done.")
+	# Sep 5: Exponentiate
+	df['eYHAT'] = np.exp(df['YHAT'])
+	df['eYHATs2'] = np.exp(df['YHATs2'])
 
-# Calculate portfolio value
-backtest['value'] = backtest['capital'] + backtest['position'] * backtest['PriceUSD']
+	# Calculate the out-of-sample R-squared
+	oos_mask = df['epoch'] >= 2
+	oos_actual = df.loc[oos_mask, 'logprice']
+	oos_predicted = df.loc[oos_mask, 'YHAT']
+	oos_predicteds2 = df.loc[oos_mask, 'YHATs2']
 
-# Plot backtest results
-# Plot backtest results below ax1
-ax5.plot(backtest['date'], backtest['capital'], label='Capital')
-ax5.set_xlabel('Date')
-ax5.set_ylabel('USD Owned')
-ax5.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, pos: format_dollars(y, pos)))
-ax5.grid(True)
-ax5.legend()
+	R2_oos = calculate_oos_r_squared(oos_actual, oos_predicted)
+	R2_oos2 = calculate_oos_r_squared(oos_actual, oos_predicteds2)
 
-ax6.plot(backtest['date'], backtest['position'], label='BTC Value')
-ax6.set_xlabel('Date')
-ax6.set_ylabel('BTC Owned')
-ax6.grid(True)
-ax6.legend()
 
-plt.tight_layout(pad=2.0, h_pad=2.0)
-plt.show()
+	print(f"Out-of-sample R-squared\n Base Model: {R2_oos} \n Phased Model: {R2_oos2}")
 
+  
+	plot_df= df[(df['date'] > plot_start_date) & (df['date'] < plot_end_date)]
+
+	plot_charts(plot_df)
+
+if __name__ == "__main__":
+    main()
